20e70dbe10f1a72741efae2b03484a1dc4b70aab
Prudhvi Chintada <prudhvisam4444@gmail.com>
2026-05-26 23:00:53 +0530

    auto update 2026-05-26 23:00:53

--- webdev/react-basics/demo/demo-react-project/src/api/github.js
+++
@@ -1,7 +0,0 @@
-export async function getGithubContributions() {
-    const response = await fetch(
-        "https://github-contributions-api.jogruber.de/v4/prudhvichin"
-    )
-
-    return response.json()
-}
