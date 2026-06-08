5a6e91d51bb158ee3cd2f040e0da6ff6c1a375e0
Prudhvi Chintada <prudhvisam4444@gmail.com>
2026-06-08 21:55:43 +0530

    auto update 2026-06-08 21:55:43

--- python/flask/app.py
+++ python/flask/app.py
@@ -1,38 +1,38 @@
-# from flask import Flask, redirect,url_for
+from flask import Flask, redirect,url_for
 
-# app = Flask(__name__)
-
-
-# def home():
-#     return "Welcome Home"
-
-# def admin():
-#     return "Welcome admin"
-
-# def student():
-#     return "Welcome student"
-
-# def staff():
-#     return "Welcome staff"
+app = Flask(__name__)
 
 
-# app.add_url_rule('/', 'home', home)
-# app.add_url_rule('/admin','admin',admin)
-# app.add_url_rule('/student','student',student)
-# app.add_url_rule('/staff','staff',staff)
+def home():
+    return "Welcome Home"
+
+def admin():
+    return "Welcome admin"
+
+def student():
+    return "Welcome student"
+
+def staff():
+    return "Welcome staff"
 
 
-
-# @app.route('/user/<name>')
-# def user(name):
-#     if name == "admin":
-#         return redirect(url_for("admin"))
-#     if name == "student":
-#         return redirect(url_for("student"))
-#     if name == "staff":
-#         return redirect(url_for("staff"))
+app.add_url_rule('/', 'home', home)
+app.add_url_rule('/admin','admin',admin)
+app.add_url_rule('/student','student',student)
+app.add_url_rule('/staff','staff',staff)
 
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route('/user/<name>')
+def user(name):
+    if name == "admin":
+        return redirect(url_for("admin"))
+    if name == "student":
+        return redirect(url_for("student"))
+    if name == "staff":
+        return redirect(url_for("staff"))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
