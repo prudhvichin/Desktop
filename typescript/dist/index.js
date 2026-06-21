e7525269ad7bf034279158e44ca76cc9233dfd5d
Prudhvi Chintada <prudhvisam4444@gmail.com>
2026-06-21 14:43:11 +0530

    auto update 2026-06-21 14:43:11

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,12 +1,23 @@
 "use strict";
+// VARIABLES EXMAMPLES
 // let name: string = 'prudhvi'
 // let salary: number = 20_000
 // let isMarried: boolean = false
 // let city: string = 'visakhapatnam'
 Object.defineProperty(exports, "__esModule", { value: true });
+// ARRAYS PRACTICE
 let nums = [1, 2, 3, 4, 5, 6];
 let grades = ['a', 'a+', 'b'];
-grades.forEach(element => {
-    console.log(element);
-});
+let fruits = ['mango', 'banana', 'apple', 'guava'];
+let marks = [89, 88, 97, 100, 90];
+// OBJECTS
+let user = {
+    name: 'prudhvi',
+    age: 22
+};
+let product = {
+    title: 'books',
+    price: 89,
+    InStock: true,
+};
 //# sourceMappingURL=index.js.map
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -6,24 +6,35 @@
 // let city: string = 'visakhapatnam'
 
 
-// ARRAYS PRACTICE
-let nums: number[] = [1, 2, 3, 4, 5, 6]
-let grades: Array<string> = ['a', 'a+', 'b']
+// // ARRAYS PRACTICE
+// let nums: number[] = [1, 2, 3, 4, 5, 6]
+// let grades: Array<string> = ['a', 'a+', 'b']
 
-let fruits: string[] = ['mango', 'banana', 'apple', 'guava']
+// let fruits: string[] = ['mango', 'banana', 'apple', 'guava']
 
-let marks: number[] = [89, 88, 97, 100, 90]
+// let marks: number[] = [89, 88, 97, 100, 90]
 
 
 
 // OBJECTS
-let user: {
-    name: string;
-    age: number;
+// let user: {
+//     name: string;
+//     age: number;
 
-} = {
-    name: 'prudhvi',
-    age: 22
-}
+// } = {
+//     name: 'prudhvi',
+//     age: 22
+// }
+
+// let product: {
+//     title: string;
+//     price: number;
+//     InStock: boolean;
+// } = {
+//     title: 'books',
+//     price: 89,
+//     InStock: true,
+// }
 
 
+//FUNCTIONS
