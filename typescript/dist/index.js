45b0312cc4554a4f2b49a93ae4601c1cde7e481e
Prudhvi Chintada <prudhvisam4444@gmail.com>
2026-06-21 11:51:04 +0530

    auto update 2026-06-21 11:51:04

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,4 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello world");
+let name = 'prudhvi';
+let salary = 20_000;
+let isMarried = false;
+let city = 'visakhapatnam';
+console.log(`${name} from ${city} at ${salary}`);
 //# sourceMappingURL=index.js.map
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world")
+let name: string = 'prudhvi'
+let salary: number = 20_000
+let isMarried: boolean = false
+let city: string = 'visakhapatnam'
+
+
+console.log(`${name} from ${city} at ${salary}`)
